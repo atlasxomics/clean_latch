5eaa994f61cbb50ff78e98015502fb36f77853a6
jpmcga <jpaulmcgann@gmail.com>
2023-08-02 15:03:59 -0400

    chore: clean up clean.py

--- wf/clean.py
+++ wf/clean.py
@@ -4,8 +4,13 @@ import pandas as pd
 import statistics
 import sys
 
+from typing import Dict, List
 
-def average_duplicates(big_list: list) -> dict:
+def average_duplicates(big_list: List[List[int]]) -> Dict[str, float]:
+  """Combine row, col, diag reduction lists; if a barcode occurs in 
+  more then one list, returns the average.
+  """
+
   barcodes_match = {}
   final = {}
   ee = []
@@ -20,28 +25,36 @@ def average_duplicates(big_list: list) -> dict:
   for i,j in barcodes_match.items():
     mean = statistics.mean(j)
     final[i] = mean
+
   return final
 
 def filter_sc(singlecell_path: str, position_path: str) -> pd.DataFrame:
-  """ reformat data, remove headers, apply custom column names for
-  dataframes, add -1 to positions, remove off tixels
+  """ Reformat data, remove headers, apply custom column names for
+  dataframes, add -1 to positions, remove off tixels.
   """
-  singlecell = pd.read_csv(singlecell_path).drop(0, axis=0)
-  positions = pd.read_csv(position_path, header=None)
-  positions.columns = ['barcode', 'on_off', 'row', 'col', 'y', 'x']
-  add_value = positions.loc[:,'barcode'].apply(lambda x: x + "-1")
-  positions['barcode'] = add_value
-  merged = pd.merge(positions.astype(object), singlecell.astype(object), how='outer', on='barcode')
-  final_value = merged[merged['on_off'] == 1]
-  return final_value
 
-def get_reductions(singlecell: pd.DataFrame, axis_id: str, deviations: int) -> pd.DataFrame:
-  """ Return table with barcode|barcode_index|adjust where "adjust" 
-  is the new value to reduce outlier lanes to; table to be used to 
+  singlecell = pd.read_csv(singlecell_path).drop(0, axis=0)
+  
+  positions = pd.read_csv(position_path, header=None)
+  positions.columns = ['barcode', 'on_off', 'row', 'col']
+  positions['barcode'] = positions.loc[:,'barcode'].apply(lambda x: x + "-1")
+
+  merged = pd.merge(positions.astype(object), singlecell.astype(object))
+  filtered = merged[merged['on_off'] == 1]
+
+  return filtered
+
+def get_reductions(
+    singlecell: pd.DataFrame,
+    axis_id: str,
+    deviations: int
+  ) -> pd.DataFrame:
+  """ Return table with barcode|barcode_index|adjust where "adjust"
+  is the new value to reduce outlier lanes to; table to be used to
   reduce fragments.tsv  
   """
+
   # calculate axis medians
-  deviations = deviations
   str_length = singlecell[axis_id].unique().tolist()
   all_indexes = {}
   for i in str_length: 
@@ -59,20 +72,24 @@ def get_reductions(singlecell: pd.DataFrame, axis_id: str, deviations: int) -> p
 
   # identify lanes more than x standard deviations above mean
   upper_limit = mean + deviations * std
-
+  
   # Filter singlecell table to only outliers
   singlecell = singlecell[singlecell[axis_id] > upper_limit]
 
   # Add "adjust" column containing value to reduce reads to
-  singlecell = singlecell.assign(adjust = lambda x: np.ceil(x.passed_filters * (mean / x[axis_id])))
+  singlecell = singlecell.assign(
+    adjust = lambda x: np.ceil(x.passed_filters * (mean / x[axis_id]))
+  )
   final = singlecell[['barcode', 'adjust']]
   return final
 
-def get_diag_reductions(singlecell: pd.DataFrame, deviations: int) -> pd.DataFrame:
-  # Return reduction table for diagonal if median of diagonal counts an outlier
-  # compared to either rows or columns.
-
-  deviations = deviations
+def get_diag_reductions(
+    singlecell: pd.DataFrame,
+    deviations: int
+  ) -> pd.DataFrame:
+  """Return reduction table for diagonal if median of diagonal counts
+  an outlier compared to either rows or columns.
+  """
 
   str_length_r = singlecell['row'].unique().tolist()
   all_indexes_r = {}
@@ -82,7 +99,6 @@ def get_diag_reductions(singlecell: pd.DataFrame, deviations: int) -> pd.DataFra
     pre_sort = singlecell.iloc[indexes]['passed_filters'].tolist()
     pre_sort.sort()
     all_indexes_r[i] = statistics.median(pre_sort)
-
 
   str_length_c = singlecell['col'].unique().tolist()
   all_indexes_c = {}
@@ -97,6 +113,7 @@ def get_diag_reductions(singlecell: pd.DataFrame, deviations: int) -> pd.DataFra
   row_std = statistics.stdev(list(all_indexes_r.values()))
   col_mean = statistics.mean(list(all_indexes_c.values()))
   col_std = statistics.stdev(list(all_indexes_c.values()))
+
   # identify limit more than x standard deviations above mean
   rows_limit = row_mean + deviations * row_std
   cols_limit = col_mean + deviations * col_std
@@ -107,15 +124,23 @@ def get_diag_reductions(singlecell: pd.DataFrame, deviations: int) -> pd.DataFra
 
   # create 'adjust' column with reads to downsample
   if diag_mean > rows_limit:
-      diag_sc['adjust'] = np.ceil(diag_sc['passed_filters'] * (row_mean / diag_mean))
+      diag_sc['adjust'] = np.ceil(
+        diag_sc['passed_filters'] * (row_mean / diag_mean)
+      )
   elif diag_mean > cols_limit:
-      diag_sc['adjust'] = np.ceil(diag_sc['passed_filters'] * (col_mean / diag_mean))
+      diag_sc['adjust'] = np.ceil(
+        diag_sc['passed_filters'] * (col_mean / diag_mean)
+      )
   else:
       diag_sc['adjust'] = diag_sc['passed_filters']
 
   return diag_sc[['barcode', 'adjust']]
 
-def combine_tables(singlecell: pd.DataFrame, deviations=1):
+def combine_tables(
+    singlecell: pd.DataFrame,
+    deviations: int=1
+  ) -> pd.DataFrame:
+
   row_singlecell = singlecell.copy()
   col_singlecell = singlecell.copy()
   dia_singlecell = singlecell.copy()
@@ -123,22 +148,35 @@ def combine_tables(singlecell: pd.DataFrame, deviations=1):
   col_reductions = get_reductions(col_singlecell, 'col', deviations)
   diag_reductions = get_diag_reductions(dia_singlecell, deviations)
 
-  # concat rows and columns
-  # If a tixel occurs twice, take the average value
-  combined_table = average_duplicates([row_reductions.values.tolist(), col_reductions.values.tolist(), diag_reductions.values.tolist()])
+  # Concat rows and columns, if a tixel occurs twice, take the average value
+  combined_table = average_duplicates([
+    row_reductions.values.tolist(),
+    col_reductions.values.tolist(),
+    diag_reductions.values.tolist()
+  ])
 
   return combined_table
 
-def clean_fragments(fragments_path: str, r_table: dict) -> pd.DataFrame:
+def clean_fragments(
+    fragments_path: str,
+    r_table: Dict[str, float]
+  ) -> pd.DataFrame:
+  """Reduce high tixels by randomly downsampling fragments.tsv
+  according to reduction table.
   """
-  Reduce high tixels by randomly downsampling fragments.tsv according to
-  reduction table  
-  """
+
   print("Loading fragments.tsv")
-  fragments = pd.read_csv(fragments_path, sep='\t', header=None, skiprows=list(range(51)))
+  fragments = pd.read_csv(
+    fragments_path,
+    sep='\t',
+    header=None,
+    skiprows=list(range(51))
+  )
   fragments.columns = ['V1', 'V2', 'V3', 'barcode', 'V4']
+
   frag_copy = fragments.copy()
   outlier_barcodes = list(r_table.keys())
+
   print("Splitting fragments.tsv")
   normal_frags = fragments[fragments['barcode'].isin(outlier_barcodes) == False]
   outlier_frags = frag_copy[frag_copy['barcode'].isin(outlier_barcodes) == True]
@@ -158,12 +196,21 @@ def clean_fragments(fragments_path: str, r_table: dict) -> pd.DataFrame:
 
   return fragments_cleaned
 
-# do stuff ---------------------------------------------------------------------
+if __name__ == '__main__':
 
-args = [i for i in sys.argv]
+  run_id = sys.args[1]
+  singlecell_path = sys.args[2]
+  position_path = sys.args[3]
+  fragments_path = sys.args[4]
+  deviations = int(sys.args[5])
 
-singlecell = filter_sc(args[1], args[2])
-reduct_dict = combine_tables(singlecell, deviations=int(args[4]))
-cleaned = clean_fragments(args[3], reduct_dict)
+  singlecell = filter_sc(singlecell_path, position_path)
+  reduct_dict = combine_tables(singlecell, deviations)
+  cleaned = clean_fragments(fragments_path, reduct_dict)
 
-cleaned.to_csv("/root/{}_fragments.tsv".format(args[0]), sep='\t', index=False, header=False)
+  cleaned.to_csv(
+    f'{run_id}_fragments.tsv',
+    sep='\t',
+    index=False,
+    header=False
+  )
