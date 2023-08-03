37718e3a6053f4ce5a715dc3cae014a1c9a4298f
jpmcga <jpaulmcgann@gmail.com>
2023-08-03 15:56:08 -0400

    fix: args to argv

--- wf/clean.py
+++ wf/clean.py
@@ -204,11 +204,11 @@ def clean_fragments(
 
 if __name__ == '__main__':
 
-  run_id = sys.args[1]
-  singlecell_path = sys.args[2]
-  position_path = sys.args[3]
-  fragments_path = sys.args[4]
-  deviations = int(sys.args[5])
+  run_id = sys.argv[1]
+  singlecell_path = sys.argv[2]
+  position_path = sys.argv[3]
+  fragments_path = sys.argv[4]
+  deviations = int(sys.argv[5])
 
   singlecell = filter_sc(singlecell_path, position_path)
   reduct_dict = combine_tables(singlecell, deviations)
